b21667b7c9581d84360bc17133f689eddb2cfe3e
Mitchell Keith Bloch <bazald@gmail.com>
2008-08-24 02:23:21 +0000

    world_to_screen and screen_to_world Matrix4fs fixed for 2D, but not yet 3D;

--- src/Zeni/Video.cpp
+++ src/Zeni/Video.cpp
@@ -52,7 +52,9 @@ namespace Zeni {
     m_icon("icons/icon.gif"),
     m_color(1.0f, 1.0f, 1.0f, 1.0f),
     m_clear_color(1.0f, 0.0f, 0.0f, 0.0f),
-    m_preview(Matrix4f::Scale(Vector3f(0.5f, -0.5f, -1.0f)) * Matrix4f::Translate(Vector3f(1.0f, -1.0f, 0.0f)))
+    m_preview(Matrix4f::Translate(Vector3f(-0.5f, -0.5f, 0.0f)) *
+      Matrix4f::Scale(Vector3f(0.5f, -0.5f, -1.0f)) *
+      Matrix4f::Translate(Vector3f(1.0f, -1.0f, 0.0f)))
   {
   }
 
