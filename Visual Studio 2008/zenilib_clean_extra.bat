da82eb5b8c35e87f68dda1480e15bb99af8c36c3
Mitchell Keith Bloch <bazald@gmail.com>
2009-07-19 23:28:41 +0000

    Last 0.3.1.1 fixes; Application_SoarSML_2008.vcproj.user added; Batch files corrected to avoid deleting both general Application_*.vcproj.user files;

--- Visual Studio 2008/zenilib_clean_extra.bat
+++ Visual Studio 2008/zenilib_clean_extra.bat
@@ -11,4 +11,11 @@ DEL /Q "*.ncb"
 
 DEL /Q /A:H "*.suo"
 
-DEL /Q "*.user"
+FOR /f %%a IN ('dir /b *.user') DO (
+  IF "%%a" NEQ "Application_2008.vcproj.user" (
+    IF "%%a" NEQ "Application_SoarSML_2008.vcproj.user" (
+      ECHO DEL /Q "%%a"
+      DEL /Q "%%a"
+    )
+  )
+)
