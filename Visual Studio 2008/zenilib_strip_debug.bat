5d6e60148fd848f6a37ec578a489318cdb0d1699
Mitchell Keith Bloch <bazald@gmail.com>
2009-08-19 23:42:49 +0000

    zenilib_clean_extra.bat renamed to zenilib_clean++.bat, which now removes debug builds as well (making zenilib_strip_debug.bat obsolete); zenilib_clean+strip_aio.bat renamed to zenilib_clean++_x86.bat;

--- Visual Studio 2008/zenilib_strip_debug.bat
+++
@@ -1,7 +0,0 @@
-FOR /F "tokens=*" %%G IN ('DIR /B /AD /S Win32\*_d') DO RMDIR /S /Q "%%G"
-FOR /F "tokens=*" %%G IN ('DIR /B /AD /S x64\*_d') DO RMDIR /S /Q "%%G"
-
-DEL /Q "..\bin\*_d.exe"
-DEL /Q "..\bin\x64\*_x64d.exe"
-DEL /Q "..\*_d.exe"
-DEL /Q "..\*_x64d.exe"
