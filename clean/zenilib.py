2afc9d4220de9e1bd4b4a40ecbc0e4371ed3e124
Mitchell Keith Bloch <bazald@gmail.com>
2009-08-25 00:57:38 +0000

    python versions of cleaning scripts added; all cleaning scripts moved to zenilib/clean/;

---
+++ clean/zenilib.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+
+import glob, os, shutil, sys
+
+auto_accept = False
+
+def isspace(c):
+  return c == ' ' or c == '\t' or c == '\r' or c == '\n'
+
+def yes_no_prompt(prompt):
+  if auto_accept:
+    return True
+  
+  print prompt + " [y/N] " ,
+  line = sys.stdin.readline()
+  print ""
+  while len(line) is not 0 and isspace(line[0]):
+    line = line[1:]
+  while len(line) is not 0 and isspace(line[len(line)-1]):
+    line = line[:len(line)-1]
+  
+  return line == "y" or line == "Y" or line == "yes" or line == "YES"
+
+def destroy_one(path):
+  if os.path.exists(path):
+    if os.path.isdir(path):
+      if yes_no_prompt("Delete directory '" + path + "'?"):
+        shutil.rmtree(path, True)
+    else:
+      if yes_no_prompt("Delete file '" + path + "'?"):
+        try:
+          os.remove(path)
+        except OSError:
+          return
+
+def destroy(path, excludes = []):
+  list = glob.glob(path)
+  for entry in list:
+    try:
+      excludes.index(entry)
+    except ValueError:
+      destroy_one(entry)
