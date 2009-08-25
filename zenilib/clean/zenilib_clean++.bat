6eb41a2c8a86887b8ac038771d57dc27cbbcbbe6
Mitchell Keith Bloch <bazald@gmail.com>
2009-08-25 02:40:17 +0000

    Cleaner scripts upgraded;

--- zenilib/clean/zenilib_clean++.bat
+++ zenilib/clean/zenilib_clean++.bat
@@ -1,20 +1,20 @@
 call zenilib_clean.bat
 
 
-DEL /Q /A:H "*.suo"
+DEL /Q /A:H "..\Visual Studio 2008\*.suo"
 
-FOR /f %%a IN ('dir /b ..\Visual Studio 2008\*.user') DO (
-  IF "%%a" NEQ "..\Visual Studio 2008\Application_2008.vcproj.user" (
-    IF "%%a" NEQ "..\Visual Studio 2008\Application_SoarSML_2008.vcproj.user" (
-      ECHO DEL /Q "%%a"
-      DEL /Q "%%a"
+FOR /F %%a IN ('dir /B "..\Visual Studio 2008\*.user"') DO (
+  IF "%%a" NEQ "Application_2008.vcproj.user" (
+    IF "%%a" NEQ "Application_SoarSML_2008.vcproj.user" (
+      ECHO DEL /Q "..\Visual Studio 2008\%%a"
+      DEL /Q "..\Visual Studio 2008\%%a"
     )
   )
 )
 
 
-FOR /F "tokens=*" %%G IN ('DIR /B /AD /S ..\Visual Studio 2008\Win32\*_d') DO RMDIR /S /Q "%%G"
-FOR /F "tokens=*" %%G IN ('DIR /B /AD /S ..\Visual Studio 2008\x64\*_d') DO RMDIR /S /Q "%%G"
+FOR /F "tokens=*" %%G IN ('DIR /B /AD /S "..\Visual Studio 2008\Win32\*_d"') DO RMDIR /S /Q "%%G"
+FOR /F "tokens=*" %%G IN ('DIR /B /AD /S "..\Visual Studio 2008\x64\*_d"') DO RMDIR /S /Q "%%G"
 
 DEL /Q "..\bin\*_d.exe"
 DEL /Q "..\bin\x64\*_x64d.exe"
