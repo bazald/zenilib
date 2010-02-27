7383553c945217405406baa71a4b637bed887eaf
Mitchell Keith Bloch <bazald@gmail.com>
2010-02-27 05:36:36 +0000

    First pass on compatibility with SDL 1.3; The basics seem to work, ignoring Video reinitialization;

--- zenilib/src/Zeni/Game.cpp
+++ zenilib/src/Zeni/Game.cpp
@@ -73,19 +73,30 @@ namespace Zeni {
           const SDL_keysym &s = event.key.keysym;
           const bool modifiers = 
 #if defined(_MACOSX)
-                                (get_key_state(SDLK_LMETA) || get_key_state(SDLK_RMETA)) &&
+#if SDL_VERSION_ATLEAST(1,3,0)
+                                (get_key_state(SDLK_LGUI) || get_key_state(SDLK_RGUI) &&
+#else
+                                (get_key_state(SDLK_LMETA) || get_key_state(SDLK_RMETA) || get_key_state(SDLK_LSUPER) || get_key_state(SDLK_RSUPER) &&
+#endif
                                 !get_key_state(SDLK_LALT) && !get_key_state(SDLK_LALT) &&
                                 !get_key_state(SDLK_LCTRL) && !get_key_state(SDLK_RCTRL) &&
 #elif defined(_WINDOWS)
                                 (get_key_state(SDLK_LALT) || get_key_state(SDLK_RALT)) &&
                                 !get_key_state(SDLK_LCTRL) && !get_key_state(SDLK_RCTRL) &&
-                                !get_key_state(SDLK_LMETA) && !get_key_state(SDLK_RMETA) &&
+#if SDL_VERSION_ATLEAST(1,3,0)
+                                !get_key_state(SDLK_LGUI) && !get_key_state(SDLK_RGUI) &&
+#else
+                                !get_key_state(SDLK_LMETA) && !get_key_state(SDLK_RMETA) && !get_key_state(SDLK_LSUPER) && !get_key_state(SDLK_RSUPER) &&
+#endif
 #else
                                 (get_key_state(SDLK_LCTRL) || get_key_state(SDLK_RCTRL)) &&
                                 !get_key_state(SDLK_LALT) && !get_key_state(SDLK_LALT) &&
-                                !get_key_state(SDLK_LMETA) && !get_key_state(SDLK_RMETA) &&
+#if SDL_VERSION_ATLEAST(1,3,0)
+                                !get_key_state(SDLK_LGUI) && !get_key_state(SDLK_RGUI) &&
+#else
+                                !get_key_state(SDLK_LMETA) && !get_key_state(SDLK_RMETA) && !get_key_state(SDLK_LSUPER) && !get_key_state(SDLK_RSUPER) &&
 #endif
-                                !get_key_state(SDLK_LSUPER) && !get_key_state(SDLK_RSUPER) &&
+#endif
                                 !get_key_state(SDLK_LSHIFT) && !get_key_state(SDLK_RSHIFT);
 
 #ifndef NDEBUG
