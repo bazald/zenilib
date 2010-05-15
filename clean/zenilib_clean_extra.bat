16aaa2b7f5fa086668d096f465b2e6a7a2a3bcc4
Mitchell Keith Bloch <bazald@gmail.com>
2010-05-15 20:07:38 +0000

    Cleaner scripts updated to support Visual Studio 2010;

--- clean/zenilib_clean_extra.bat
+++ clean/zenilib_clean_extra.bat
@@ -2,12 +2,21 @@ call zenilib_clean.bat
 
 
 DEL /Q /A:H "..\Visual Studio 2008\*.suo"
+DEL /Q /A:H "..\Visual Studio 2010\*.suo"
 
 FOR /F "tokens=*" %%G IN ('DIR /B "..\Visual Studio 2008\*.user"') DO (
-  IF "%%G" NEQ "Application_2008.vcproj.user" (
-    IF "%%G" NEQ "Application_SoarSML_2008.vcproj.user" (
+  IF "%%G" NEQ "Application.vcproj.user" (
+    IF "%%G" NEQ "Application_SoarSML.vcproj.user" (
       ECHO DEL /Q "..\Visual Studio 2008\%%G"
       DEL /Q "..\Visual Studio 2008\%%G"
+    )
+  )
+)
+FOR /F "tokens=*" %%G IN ('DIR /B "..\Visual Studio 2010\*.user"') DO (
+  IF "%%G" NEQ "Application.vcxproj.user" (
+    IF "%%G" NEQ "Application_SoarSML.vcxproj.user" (
+      ECHO DEL /Q "..\Visual Studio 2010\%%G"
+      DEL /Q "..\Visual Studio 2010\%%G"
     )
   )
 )
@@ -15,6 +24,8 @@ FOR /F "tokens=*" %%G IN ('DIR /B "..\Visual Studio 2008\*.user"') DO (
 
 FOR /F "tokens=*" %%G IN ('DIR /B /AD /S "..\Visual Studio 2008\Win32\*_d"') DO RMDIR /S /Q "%%G"
 FOR /F "tokens=*" %%G IN ('DIR /B /AD /S "..\Visual Studio 2008\x64\*_d"') DO RMDIR /S /Q "%%G"
+FOR /F "tokens=*" %%G IN ('DIR /B /AD /S "..\Visual Studio 2010\Win32\*_d"') DO RMDIR /S /Q "%%G"
+FOR /F "tokens=*" %%G IN ('DIR /B /AD /S "..\Visual Studio 2010\x64\*_d"') DO RMDIR /S /Q "%%G"
 
 DEL /Q "..\bin\*_d.exe"
 DEL /Q "..\bin\x64\*_x64d.exe"
--- clean/zenilib_clean_extra.py
+++ clean/zenilib_clean_extra.py
@@ -9,10 +9,14 @@ def main():
   zenilib.auto_accept = True
   
   zenilib.destroy("Visual Studio 2008/*.suo")
-  zenilib.destroy("Visual Studio 2008/*.user", ["Application_2008.vcproj.user", "Application_SoarSML_2008.vcproj.user"])
+  zenilib.destroy("Visual Studio 2008/*.user", ["Application.vcproj.user", "Application_SoarSML.vcproj.user"])
+  zenilib.destroy("Visual Studio 2010/*.suo")
+  zenilib.destroy("Visual Studio 2010/*.user", ["Application.vcxproj.user", "Application_SoarSML.vcxproj.user"])
   
   zenilib.destroy("Visual Studio 2008/Win32/*_d")
   zenilib.destroy("Visual Studio 2008/x64/*_d")
+  zenilib.destroy("Visual Studio 2010/Win32/*_d")
+  zenilib.destroy("Visual Studio 2010/x64/*_d")
   
   zenilib.destroy("bin/*_d.exe")
   zenilib.destroy("bin/x64/*_x64d.exe")
