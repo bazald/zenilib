9e107029dfcfcc427808cf97719dac2ad2c174bd
Mitchell Keith Bloch <bazald@gmail.com>
2010-07-11 04:28:21 +0000

    Upgrades to the Xcode project (and Frameworks) to support x64 builds; copy_game_files.sh made more efficient, improving Xcode build times; Version increment to 0.3.3.1;

--- Xcode/copy_game_files.sh
+++ Xcode/copy_game_files.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-cd ..
+cd ../
 
 for App in *.app; do
-  mkdir -p "$App/Contents/zenilib"
-
-  FILES="*"
-  for File in $FILES; do
-    AppName=`echo $File | grep -v ".app"`
-    if [ "$AppName" != "" ] && [ "$File" != "Doxyfile" ] && [ "$File" != "Frameworks" ] && [ "$File" != "SConstruct" ] && [ "$File" != "Visual Studio 2008" ] && [ "$File" != "Xcode 3.1.2" ] && [ "$File" != "clean" ] && [ "$File" != "include" ] && [ "$File" != "src" ]; then
-      echo "rsync -r --delete \"$File\" \"$App/Contents/zenilib/\""
-      rsync -r --delete "$File" "$App/Contents/zenilib/"
-    fi
-  done
+  rsync -rlt --delete \
+    --exclude "*.app" \
+    --exclude clean/ \
+    --exclude "Code Blocks/" \
+    --exclude Doxyfile \
+    --exclude Frameworks/ \
+    --exclude include/ \
+    --exclude SConstruct \
+    --exclude src/ \
+    --exclude "Visual Studio 2008/" \
+    --exclude "Visual Studio 2010/" \
+    --exclude Xcode/ \
+    ./ "$App/Contents/zenilib/"
 done
