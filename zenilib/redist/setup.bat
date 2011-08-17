eb00ea1ad4de5decde96e9fdf757f30a12b1b9b3
Mitchell Keith Bloch <bazald@gmail.com>
2011-08-17 06:53:11 +0000

    Version increment to 0.4.0.0; DOxygen updates and fixes; NSIS helpers updated and made more minimal;

--- zenilib/redist/setup.bat
+++ zenilib/redist/setup.bat
@@ -1,6 +1,1 @@
-"%~dp0visualc\vcredist_x86_9.0.30729.17.exe" /quiet /passive /C /T:%TEMP%
-"%~dp0visualc\vcredist_x64_9.0.30729.17.exe" /quiet /passive /C /T:%TEMP%
-"%~dp0visualc\vcredist_x86_10.0.30319.01.exe" /q
-"%~dp0visualc\vcredist_x64_10.0.30319.01.exe" /q
-"%~dp0directx\DXSETUP.exe" /silent
 "%~dp0openal\oalinst_2.0.7.0.exe" /silent
