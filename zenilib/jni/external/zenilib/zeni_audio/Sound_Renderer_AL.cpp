2426ac77043bd0f6d8b1eeae96b3c41645960663
Mitchell Keith Bloch <bazald@gmail.com>
2012-11-30 04:17:30 +0000

    Sound_Stream_AL fix for Mac OS X;

--- zenilib/jni/external/zenilib/zeni_audio/Sound_Renderer_AL.cpp
+++ zenilib/jni/external/zenilib/zeni_audio/Sound_Renderer_AL.cpp
@@ -135,6 +135,8 @@ namespace Zeni {
     g_alSourcePause = (alSourcePause_fcn)::alSourcePause;
     g_alSourcePlay = (alSourcePlay_fcn)::alSourcePlay;
     g_alSourceStop = (alSourceStop_fcn)::alSourceStop;
+    g_alSourceQueueBuffers = (alSourceQueueBuffers_fcn)::g_alSourceQueueBuffers;
+    g_alSourceUnqueueBuffers = (alSourceUnqueueBuffers_fcn)::g_alSourceUnqueueBuffers;
 #endif
 
     m_device = alcOpenDevice()(0);
