07ded13ec62dedf8f943281ff66fddc22f857bfb
Mitchell Keith Bloch <bazald@gmail.com>
2013-08-07 09:11:59 +0000

    Indexed elements for D3DX9;

--- zenilib/jni/application/modern.h
+++ zenilib/jni/application/modern.h
@@ -22,7 +22,7 @@ GLfloat vertices[12] = {  0.0f,   0.0f,
 GLfloat colors[12] = {1.0f, 0.0f, 0.0f, 1.0f,
                       0.0f, 1.0f, 0.0f, 1.0f,
                       0.0f, 0.0f, 1.0f, 1.0f};
-GLuint indices[3] = {0, 1, 2};
+GLushort indices[3] = {0, 1, 2};
 
 class Modern_State : public Gamestate_Base {
   Modern_State(const Modern_State &);
@@ -38,7 +38,7 @@ public:
     vertexDecl(0),
     vertexBuffer(0),
     colorBuffer(0),
-    m_use_shader(false)
+    indexBuffer(0)
   {
     set_pausable(true);
     
@@ -102,15 +102,23 @@ public:
         throw Error("Lock failed.");
       memcpy(data, colors, sizeof(colors));
       colorBuffer->Unlock();
+
+      vdx.get_d3d_device()->CreateIndexBuffer(sizeof(indices), D3DUSAGE_WRITEONLY, D3DFMT_INDEX16, D3DPOOL_MANAGED, &indexBuffer, 0);
+      if(FAILED(indexBuffer->Lock(0, 0, &data, 0)))
+        throw Error("Lock failed.");
+      memcpy(data, indices, sizeof(indices));
+      indexBuffer->Unlock();
     }
   }
 
   ~Modern_State() {
     if(dynamic_cast<Video_DX9 *>(&get_Video())) {
-      if(vertexBuffer)
-        vertexBuffer->Release();
+      if(indexBuffer)
+        indexBuffer->Release();
       if(colorBuffer)
         colorBuffer->Release();
+      if(vertexBuffer)
+        vertexBuffer->Release();
       if(vertexDecl)
         vertexDecl->Release();
     }
@@ -124,14 +132,15 @@ private:
     vr.set_backface_culling(false);
     
     const Matrix4f modelViewMatrix = Matrix4f::Orthographic(0.0f, 800.0f, 600.0f, 0.0f, -1.0f, 1.0f);
+    const bool use_shaders = get_Game().get_key_state(SDLK_SPACE);
 
-    if(get_Game().get_key_state(SDLK_SPACE)) {
+    if(use_shaders) {
       vr.set_program(*m_program);
 
       if(Program_GL_Fixed * program = dynamic_cast<Program_GL_Fixed *>(m_program.get())) {
         GLuint modelViewProj = glGetUniformLocationARB(program->get(), "modelViewProj");
         glUniformMatrix4fvARB(modelViewProj, 1, GL_FALSE, reinterpret_cast<const GLfloat *>(&modelViewMatrix));
-        
+
         glBindBufferARB(GL_ARRAY_BUFFER_ARB, buffers[0]);
         glVertexAttribPointerARB(a_position, 2, GL_FLOAT, GL_FALSE, 0, 0);
         glEnableVertexAttribArrayARB(a_position);
@@ -141,7 +150,7 @@ private:
         glEnableVertexAttribArrayARB(a_color);
         
         glBindBufferARB(GL_ELEMENT_ARRAY_BUFFER_ARB, buffers[2]);
-        glDrawElements(GL_TRIANGLE_STRIP, 3, GL_UNSIGNED_INT, 0);
+        glDrawElements(GL_TRIANGLE_STRIP, 3, GL_UNSIGNED_SHORT, 0);
 
         glDisableVertexAttribArrayARB(a_position);
         glDisableVertexAttribArrayARB(a_color);
@@ -151,7 +160,7 @@ private:
       else if(Program_GL_Shader * program = dynamic_cast<Program_GL_Shader *>(m_program.get())) {
         GLuint modelViewProj = glGetUniformLocation(program->get(), "modelViewProj");
         glUniformMatrix4fv(modelViewProj, 1, GL_FALSE, reinterpret_cast<const GLfloat *>(&modelViewMatrix));
-        
+
         glBindBuffer(GL_ARRAY_BUFFER, buffers[0]);
         glVertexAttribPointer(a_position, 2, GL_FLOAT, GL_FALSE, 0, 0);
         glEnableVertexAttribArray(a_position);
@@ -161,7 +170,7 @@ private:
         glEnableVertexAttribArray(a_color);
         
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, buffers[2]);
-        glDrawElements(GL_TRIANGLE_STRIP, 3, GL_UNSIGNED_INT, 0);
+        glDrawElements(GL_TRIANGLE_STRIP, 3, GL_UNSIGNED_SHORT, 0);
 
         glDisableVertexAttribArray(a_position);
         glDisableVertexAttribArray(a_color);
@@ -174,49 +183,42 @@ private:
 
         if(FAILED(vsdx.get_constant_table()->SetMatrix(vdx.get_d3d_device(), "$_modelViewProj", reinterpret_cast<const D3DXMATRIX *>(&modelViewMatrix))))
           throw Error("Setting modelViewProj failed.");
-
-        vdx.get_d3d_device()->SetVertexDeclaration(vertexDecl);
-        vdx.get_d3d_device()->SetStreamSource(0, vertexBuffer, 0, 2 * sizeof(float));
-        vdx.get_d3d_device()->SetStreamSource(1, colorBuffer, 0, 4 * sizeof(float));
-
-        vdx.get_d3d_device()->DrawPrimitive(D3DPT_TRIANGLESTRIP, 0, 1);
-        
-        vdx.get_d3d_device()->SetStreamSource(0, 0, 0, 0);
-        vdx.get_d3d_device()->SetStreamSource(1, 0, 0, 0);
-        vdx.get_d3d_device()->SetVertexDeclaration(0);
       }
-
-      vr.unset_program();
     }
     else {
       vr.set_view_matrix(modelViewMatrix);
       vr.set_projection_matrix(Matrix4f::Identity());
-
-      if(dynamic_cast<Video_DX9 *>(&get_Video())) {
-        Video_DX9 &vdx = dynamic_cast<Video_DX9 &>(get_Video());
-
-        vdx.get_d3d_device()->SetVertexDeclaration(vertexDecl);
-        vdx.get_d3d_device()->SetStreamSource(0, vertexBuffer, 0, 2 * sizeof(float));
-        vdx.get_d3d_device()->SetStreamSource(1, colorBuffer, 0, 4 * sizeof(float));
-
-        vdx.get_d3d_device()->DrawPrimitive(D3DPT_TRIANGLESTRIP, 0, 1);
-        
-        vdx.get_d3d_device()->SetStreamSource(0, 0, 0, 0);
-        vdx.get_d3d_device()->SetStreamSource(1, 0, 0, 0);
-        vdx.get_d3d_device()->SetVertexDeclaration(0);
-      }
-      else {
-        glEnableClientState(GL_VERTEX_ARRAY);
-        glVertexPointer(2, GL_FLOAT, 0, vertices);
-        glEnableClientState(GL_COLOR_ARRAY);
-        glColorPointer(4, GL_FLOAT, 0, colors);
-      
-        glDrawElements(GL_TRIANGLE_STRIP, 3, GL_UNSIGNED_INT, indices);
-      
-        glDisableClientState(GL_VERTEX_ARRAY);
-        glDisableClientState(GL_COLOR_ARRAY);
-      }
     }
+
+    if(Program_DX9 * program = dynamic_cast<Program_DX9 *>(m_program.get())) {
+      Video_DX9 &vdx = dynamic_cast<Video_DX9 &>(get_Video());
+
+      vdx.get_d3d_device()->SetVertexDeclaration(vertexDecl);
+      vdx.get_d3d_device()->SetStreamSource(0, vertexBuffer, 0, 2 * sizeof(float));
+      vdx.get_d3d_device()->SetStreamSource(1, colorBuffer, 0, 4 * sizeof(float));
+      vdx.get_d3d_device()->SetIndices(indexBuffer);
+
+      vdx.get_d3d_device()->DrawIndexedPrimitive(D3DPT_TRIANGLESTRIP, 0, 0, 3, 0, 1);
+        
+      vdx.get_d3d_device()->SetIndices(0);
+      vdx.get_d3d_device()->SetStreamSource(0, 0, 0, 0);
+      vdx.get_d3d_device()->SetStreamSource(1, 0, 0, 0);
+      vdx.get_d3d_device()->SetVertexDeclaration(0);
+    }
+    else if(!use_shaders) {
+      glEnableClientState(GL_VERTEX_ARRAY);
+      glVertexPointer(2, GL_FLOAT, 0, vertices);
+      glEnableClientState(GL_COLOR_ARRAY);
+      glColorPointer(4, GL_FLOAT, 0, colors);
+      
+      glDrawElements(GL_TRIANGLE_STRIP, 3, GL_UNSIGNED_SHORT, indices);
+      
+      glDisableClientState(GL_VERTEX_ARRAY);
+      glDisableClientState(GL_COLOR_ARRAY);
+    }
+
+    if(use_shaders)
+      vr.unset_program();
   }
   
   shared_ptr<Shader> m_vertex_shader;
@@ -229,6 +231,5 @@ private:
   LPDIRECT3DVERTEXDECLARATION9 vertexDecl;
   LPDIRECT3DVERTEXBUFFER9 vertexBuffer;
   LPDIRECT3DVERTEXBUFFER9 colorBuffer;
-
-  bool m_use_shader;
+  LPDIRECT3DINDEXBUFFER9 indexBuffer;
 };
